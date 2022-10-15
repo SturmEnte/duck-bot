5ed8987e53fd7d5e9d8b8c8664f8ac49b926bcce
SturmEnte <sturmente@proton.me>
2022-10-15 19:58:58 +0200

    Bot login

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Duck Bot
+
+## How to use the bot
+
+Create an environment variable called "TOKEN" with the token of your bot as the value
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+require("dotenv/config");
+require("colors");
+
+const { Client } = require("discord.js");
+
+const client = new Client({ intents: ["GuildVoiceStates"] });
+
+client.on("ready", () => {
+	console.log("Logged in as", client.user.tag.cyan);
+});
+
+client.login(process.env.TOKEN);
