76a22ddcd0198b285bf02562314fe065f5d1ab98
SturmEnte <sturmente@proton.me>
2022-10-17 19:57:34 +0200

    Added join command

---
+++ src/commands/join.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction } from "discord.js";
+import { joinVoiceChannel } from "@discordjs/voice";
+
+module.exports.init = () => {};
+
+module.exports.execute = async (interaction: CommandInteraction) => {
+	const member = await interaction.guild?.members.fetch(interaction.user.id);
+
+	if (!interaction.guild) {
+		interaction.reply("The music commands only work in guilds");
+		return;
+	}
+
+	if (!member?.voice.channelId) {
+		interaction.reply("You need to be in a channel to use this command");
+		console.log(member?.voice);
+		return;
+	}
+
+	joinVoiceChannel({
+		guildId: String(interaction.guild.id),
+		channelId: String(member.voice.channelId),
+		adapterCreator: interaction.guild.voiceAdapterCreator,
+	});
+
+	interaction.reply("Joined voice channel");
+};
+
+module.exports.command = {
+	name: "join",
+	description: "Joins your current voice channel",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import { CommandInteraction } from "discord.js";
-
-require("dotenv/config");
-require("colors");
-
-const { Client, InteractionType } = require("discord.js");
+import "colors";
+import "dotenv/config";
+import { Client, GuildMember, Interaction, InteractionType } from "discord.js";
 
 const commands: any = {
 	ping: require("./commands/ping"),
+	join: require("./commands/join"),
 };
 
-const client = new Client({ intents: ["GuildVoiceStates"] });
+const client: Client = new Client({
+	intents: ["GuildMembers", "GuildVoiceStates"],
+});
 
 client.on("ready", () => {
-	console.log("Logged in as", client.user.tag.cyan);
+	console.log("Logged in as", client.user?.tag.cyan);
 
 	// Register Commands
 	for (const key in commands) {
 		commands[key].init();
-		client.application.commands.create(commands[key].command).then(() => {
+		client.application?.commands.create(commands[key].command).then(() => {
 			console.log("Registered", commands[key].command.name.cyan, "command");
 		});
 	}
 });
 
-client.on("interactionCreate", (interaction: CommandInteraction) => {
+client.on("interactionCreate", (interaction: Interaction) => {
 	if (interaction.type == InteractionType.ApplicationCommand) {
 		commands[interaction.commandName].execute(interaction);
 	}
