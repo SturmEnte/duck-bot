f551b8908443ebc839f7ae3dc18b20d621d674f3
SturmEnte <sturmente@proton.me>
2022-10-19 20:01:27 +0200

    Fixed the join command

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,18 +1,27 @@
 import { CommandInteraction } from "discord.js";
 
+import VoiceManager from "../manager/VoiceManager";
 import joinChannel from "../utility/joinChannel";
+import Global from "../types/Global";
 
-let global: any;
+let global: Global;
 
-export function init(g: any) {
+export function init(g: Global) {
 	global = g;
 }
 
 export async function execute(interaction: CommandInteraction) {
-	let connection = await joinChannel(interaction);
-	if (!connection) return;
-	global.connection = connection;
-	interaction.reply("Joined voice channel");
+	if (!interaction.guildId) return;
+
+	if (!global.queueMangers.has(interaction.guildId)) {
+		const connection = await joinChannel(interaction);
+		if (!connection) return;
+		global.queueMangers.set(interaction.guildId, new VoiceManager(interaction, connection));
+		interaction.reply("Joined the voice channel");
+		return;
+	} else {
+		interaction.reply("I'm alreayd in a channel");
+	}
 }
 
 export const command = {
