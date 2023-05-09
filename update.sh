fed47b6d322d60a90c677e457f179392225315d2
Jonas <sturmente@proton.me>
2023-05-09 22:14:25 +0200

    Added prints to the update script

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+echo "Starting update"
+echo "Pull from GitHub"
 git pull
+echo "Install npm packages"
 npm i
+echo "Build application"
 npm run build
 cd build
+echo "Build Docker image"
 docker build -t duck-bot .
+echo "Updated"
