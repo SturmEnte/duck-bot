690d01beb9a0c5ac4e8f7990b2f6e6ecb7d92078
Jonas <sturmente@proton.me>
2023-05-09 21:29:43 +0200

    Load commands

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import "dotenv/config";
 import { ApplicationCommandDataResolvable, ApplicationCommandOptionType, Client, InteractionType } from "discord.js";
+
+import Command from "./interfaces/Command";
+const commands: Command[] = [
+];
+
 const client: Client = new Client({
 	intents: ["GuildMembers"],
 });
+
 client.on("ready", () => {
 	console.log("Logged in as", client.user?.tag);
+
+	// Load commands
+	let cmds: ApplicationCommandDataResolvable[] = [];
+	commands.forEach((command) => {
+		cmds.push(command.data);
+	});
+	client.application?.commands.set(cmds);
 });
 client.login(process.env.TOKEN);
