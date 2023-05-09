273a6f82d73a19f94a808b70c522a391b03f50b1
Jonas <sturmente@proton.me>
2023-05-09 21:29:09 +0200

    Login client

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import "dotenv/config";
+import { ApplicationCommandDataResolvable, ApplicationCommandOptionType, Client, InteractionType } from "discord.js";
+const client: Client = new Client({
+	intents: ["GuildMembers"],
+});
+client.on("ready", () => {
+	console.log("Logged in as", client.user?.tag);
+});
+client.login(process.env.TOKEN);
