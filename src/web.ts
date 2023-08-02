faa9255e8a6954f21af1a7bc46aaca124acea563
Jonas <sturmente@proton.me>
2023-08-02 17:44:32 +0200

    Check if user is authorized

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,8 @@
 import { Client } from "discord.js";
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
+
+import Token from "./models/Token";
 
 let client: Client;
 let app: Application = express();
@@ -13,10 +16,38 @@ function setup(newClient: Client) {
 
 import auth from "./web/auth";
 
+app.use(cookieParser());
+
 app.use("/auth", auth);
 
-app.get("*", (req, res) => {
-	res.send(client.user.username);
+app.all("*", async (req, res, next) => {
+	console.log(req.cookies);
+
+	if (!req.cookies.token) {
+		if (req.url.includes("api")) {
+			res.status(401).json({ error: "No token" });
+		} else {
+			res.redirect(process.env.OAUTH2_URL);
+		}
+
+		return;
+	}
+
+	if (!(await Token.exists({ token: req.cookies.token }))) {
+		if (req.url.includes("api")) {
+			res.status(401).json({ error: "Invalid token" });
+		} else {
+			res.redirect(process.env.OAUTH2_URL);
+		}
+
+		return;
+	}
+
+	next();
+});
+
+app.all("*", (req, res) => {
+	res.status(404).send("Not found");
 });
 
 export default setup;
