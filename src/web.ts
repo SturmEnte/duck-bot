3f57a732b276c265050d84c3e26548baa27b6862
Jonas <sturmente@proton.me>
2023-08-03 19:44:29 +0200

    Moved the guilds page from the web file into its own file

--- src/web.ts
+++ src/web.ts
@@ -5,100 +5,59 @@ import cookieParser from "cookie-parser";
 
 import Token from "./models/Token";
 
-import accessTokenManager from "./utils/accessTokenManager";
-import getUserInfo from "./utils/getUserInfo";
-import getUserGuilds from "./utils/getUserGuilds";
+import AccessTokenManager from "./utils/accessTokenManager";
 
 import auth from "./web/auth";
+import guilds from "./web/guilds";
 
 let client: Client;
 let app: Application = express();
 
+const accessTokenManager: AccessTokenManager = new AccessTokenManager();
+
 function setup(newClient: Client) {
 	client = newClient;
+
+	app.use(express.static(path.join(__dirname, "../", "public")));
+	app.use(cookieParser());
+
+	app.set("view engine", "ejs");
+
+	app.use("/auth", auth);
+
+	app.all("*", async (req, res, next) => {
+		if (!req.cookies.token) {
+			if (req.url.includes("api")) {
+				res.status(401).json({ error: "No token" });
+			} else {
+				res.redirect(process.env.OAUTH2_URL);
+			}
+
+			return;
+		}
+
+		if (!(await Token.exists({ token: req.cookies.token }))) {
+			if (req.url.includes("api")) {
+				res.status(401).json({ error: "Invalid token", oauth2_url: process.env.OAUTH2_URL });
+			} else {
+				res.redirect(process.env.OAUTH2_URL);
+			}
+
+			return;
+		}
+
+		next();
+	});
+
+	app.use("/", guilds(client, accessTokenManager));
+
+	app.all("*", (req, res) => {
+		res.status(404).send("Not found");
+	});
+
 	app.listen(process.env.PORT, () => {
 		console.log("Started web server");
 	});
 }
-
-app.use(express.static(path.join(__dirname, "../", "public")));
-app.use(cookieParser());
-
-app.set("view engine", "ejs");
-
-app.use("/auth", auth);
-
-app.all("*", async (req, res, next) => {
-	if (!req.cookies.token) {
-		if (req.url.includes("api")) {
-			res.status(401).json({ error: "No token" });
-		} else {
-			res.redirect(process.env.OAUTH2_URL);
-		}
-
-		return;
-	}
-
-	if (!(await Token.exists({ token: req.cookies.token }))) {
-		if (req.url.includes("api")) {
-			res.status(401).json({ error: "Invalid token", oauth2_url: process.env.OAUTH2_URL });
-		} else {
-			res.redirect(process.env.OAUTH2_URL);
-		}
-
-		return;
-	}
-
-	next();
-});
-
-app.get("/", async (req, res) => {
-	const user_id = (await Token.findOne({ token: req.cookies.token })).user_id;
-	const access_token = await accessTokenManager.getToken(user_id).catch((err) => {
-		console.log("Error while getting access token:\n", err);
-	});
-
-	if (!access_token) {
-		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get access token" });
-		return;
-	}
-
-	const userInfo = await getUserInfo(user_id, access_token).catch((err) => {
-		console.log("Error while getting user info: \n", err);
-	});
-
-	if (!userInfo) {
-		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get user info" });
-		return;
-	}
-
-	let guilds: Array<any> | undefined = await getUserGuilds(user_id, access_token).catch((err) => {
-		console.log("Error while getting user's guilds: \n", err);
-	});
-
-	if (!guilds) {
-		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get guilds" });
-		return;
-	}
-
-	// Remove all guilds that the bot is not a member of
-	guilds = guilds.filter((guild) => {
-		if (client.guilds.cache.has(guild.id)) return true;
-		return false;
-	});
-
-	// Remove all guilds that the user is not an administrator on
-	guilds = guilds.filter((guild) => {
-		if ((guild.permissions & 0x8) == 8) return true;
-		console.log(guild.permissions);
-		return false;
-	});
-
-	res.render("guilds", { guilds, user: userInfo });
-});
-
-app.all("*", (req, res) => {
-	res.status(404).send("Not found");
-});
 
 export default setup;
---
+++ src/web/guilds.ts
@@ -0,0 +1,64 @@
+import { Router } from "express";
+import { Client } from "discord.js";
+
+import Token from "../models/Token";
+
+import getUserInfo from "../utils/getUserInfo";
+import getUserGuilds from "../utils/getUserGuilds";
+import AccessTokenManager from "../utils/accessTokenManager";
+
+let client: Client;
+let accessTokenManager: AccessTokenManager;
+
+const router = Router();
+
+export default function init(newClient: Client, newAccessTokenManager: AccessTokenManager) {
+	client = newClient;
+	accessTokenManager = newAccessTokenManager;
+	return router;
+}
+
+router.get("/", async (req, res) => {
+	const user_id = (await Token.findOne({ token: req.cookies.token })).user_id;
+	const access_token = await accessTokenManager.getToken(user_id).catch((err) => {
+		console.log("Error while getting access token:\n", err);
+	});
+
+	if (!access_token) {
+		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get access token" });
+		return;
+	}
+
+	const userInfo = await getUserInfo(user_id, access_token).catch((err) => {
+		console.log("Error while getting user info: \n", err);
+	});
+
+	if (!userInfo) {
+		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get user info" });
+		return;
+	}
+
+	let guilds: Array<any> | undefined = await getUserGuilds(user_id, access_token).catch((err) => {
+		console.log("Error while getting user's guilds: \n", err);
+	});
+
+	if (!guilds) {
+		res.render("error", { url: process.env.OAUTH2_URL, error: "Failed to get guilds" });
+		return;
+	}
+
+	// Remove all guilds that the bot is not a member of
+	guilds = guilds.filter((guild) => {
+		if (client.guilds.cache.has(guild.id)) return true;
+		return false;
+	});
+
+	// Remove all guilds that the user is not an administrator on
+	guilds = guilds.filter((guild) => {
+		if ((guild.permissions & 0x8) == 8) return true;
+		console.log(guild.permissions);
+		return false;
+	});
+
+	res.render("guilds", { guilds, user: userInfo });
+});
