d7e9a5e0b22c28465cfa283cb0cd36c694314318
Jonas <sturmente@proton.me>
2023-08-24 00:41:04 +0200

    Implemented timeout properly in frontend

--- public/guild.js
+++ public/guild.js
@@ -1,15 +1,52 @@
-function timeout(targetId) {
+const curtain = document.getElementById("curtain");
+const timeoutPopup = document.getElementById("timeout-popup");
+const timeoutDuration = document.getElementById("timeout-duration");
+const timeoutButton = document.getElementById("timeout-button");
+
+let currentPopup = undefined;
+let timeoutPopupData = {
+	guildId: undefined,
+	targetid: undefined,
+};
+
+curtain.onclick = () => {
+	console.log("Click");
+	switch (currentPopup) {
+		case "timeout":
+			timeoutPopup.style.display = "none";
+			timeoutDuration.value = undefined;
+		default:
+			curtain.style.display = "none";
+	}
+};
+
+function showTimeoutPopup(targetId) {
 	console.log("Timeout");
 	console.log("Target:", targetId);
 
-	let guildId;
+	currentPopup = "timeout";
+
+	curtain.style.display = "flex";
+	timeoutPopup.style.display = "flex";
+
 	const urlParts = location.href.split("/");
 
 	if (location.href.endsWith("/")) {
-		guildId = urlParts[urlParts.length - 2];
+		timeoutPopupData.guildId = urlParts[urlParts.length - 2];
 	} else {
-		guildId = urlParts[urlParts.length - 1];
+		timeoutPopupData.guildId = urlParts[urlParts.length - 1];
 	}
+
+	timeoutPopupData.targetid = targetId;
+}
+
+timeoutButton.onclick = () => {
+	if (timeoutDuration.value <= 0) {
+		alert("The timeout duration must be higher than 0");
+		return;
+	}
+
+	timeoutButton.setAttribute("disabled", "");
 
 	fetch("/api/timeout", {
 		method: "post",
@@ -18,9 +55,23 @@ function timeout(targetId) {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			guild_id: guildId,
-			user_id: targetId,
+			guild_id: timeoutPopupData.guildId,
+			user_id: timeoutPopupData.targetid,
 			duration: 10000,
 		}),
-	});
-}
+	})
+		.then(() => {
+			timeoutPopup.style.display = "none";
+			curtain.style.display = "none";
+			timeoutButton.removeAttribute("disabled");
+		})
+		.catch((err) => {
+			console.log(err);
+			alert("Failed to timeout member");
+			timeoutButton.removeAttribute("disabled");
+		});
+};
+
+timeoutPopup.onclick = (event) => {
+	event.stopPropagation();
+};
