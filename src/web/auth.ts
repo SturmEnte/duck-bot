9998806dc44549e2e2c03db2b608c2328d7d4c16
Jonas <sturmente@proton.me>
2024-05-31 18:26:08 +0200

    Fixed website login issue

--- src/web/auth.ts
+++ src/web/auth.ts
@@ -17,13 +17,16 @@ router.get("/", async (req, res) => {
 
 	const body = new URLSearchParams();
 
-	body.append("client_id", process.env.CLIENT_ID);
-	body.append("client_secret", process.env.CLIENT_SECRET);
 	body.append("grant_type", "authorization_code");
 	body.append("code", code);
 	body.append("redirect_uri", process.env.REDIRECT_URI);
 
-	const authResponse = await axios.post("https://discord.com/api/v10/oauth2/token", body);
+	const authResponse = await axios.post("https://discord.com/api/v10/oauth2/token", body, {
+		auth: {
+			username: process.env.CLIENT_ID,
+			password: process.env.CLIENT_SECRET,
+		},
+	});
 
 	if (!authResponse.data.refresh_token || !authResponse.data.access_token) {
 		res.status(500).send("Authorization failed. Error: \n" + authResponse.data.error);
@@ -44,10 +47,7 @@ router.get("/", async (req, res) => {
 	}
 
 	if (await OAuth2.exists({ user_id: identifyReponse.data.id })) {
-		await OAuth2.findOneAndUpdate(
-			{ user_id: identifyReponse.data.id },
-			{ refresh_token: authResponse.data.refresh_token, scope: authResponse.data.scope }
-		);
+		await OAuth2.findOneAndUpdate({ user_id: identifyReponse.data.id }, { refresh_token: authResponse.data.refresh_token, scope: authResponse.data.scope });
 	} else {
 		await OAuth2.create({ refresh_token: authResponse.data.refresh_token, scope: authResponse.data.scope, user_id: identifyReponse.data.id });
 	}
