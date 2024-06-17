77d88e911f8cc923ec204a25ec1be70c822e2c4d
Jonas <sturmente@proton.me>
2024-06-17 16:26:10 +0200

    Added rock-paper-sissors command info

--- src/commands.ts
+++ src/commands.ts
@@ -12,129 +12,160 @@ import silentUnban from "./commands/silent-ban/unban";
 import addChannelMessageKeeper from "./commands/message-keeper/addChannel";
 import removeChannelMessageKeeper from "./commands/message-keeper/removeChannel";
 
+import rockPaperSissors from "./commands/rock-paper-sissors";
+
 const commands: Command[] = [
-	{
-		run: rename,
-		data: {
-			name: "rename",
-			description: "Rename a user",
-			options: [
-				{
-					name: "user",
-					description: "The user you want to rename",
-					type: ApplicationCommandOptionType.User,
-					required: true,
-				},
-				{
-					name: "nickname",
-					description: "The new nickname",
-					type: ApplicationCommandOptionType.String,
-					required: true,
-				},
-			],
-		},
-	},
-	{
-		run: createJoinLeaveMessage,
-		data: {
-			name: "create-join-leave-message",
-			description: "Join messages",
-			options: [
-				{
-					name: "type",
-					description: "Join, Leave, Kick, Ban",
-					type: ApplicationCommandOptionType.String,
-					choices: [
-						{ name: "join", value: "join" },
-						{ name: "leave", value: "leave" },
-						{ name: "kick", value: "kick" },
-						{ name: "ban", value: "ban" },
-					],
-					required: true,
-				},
-				{
-					name: "channel",
-					description: "The channel the message should be send to",
-					type: ApplicationCommandOptionType.Channel,
-					required: true,
-				},
-				{
-					name: "message",
-					description: "{{user}} - username",
-					type: ApplicationCommandOptionType.String,
-					required: true,
-				},
-			],
-		},
-	},
-	{
-		run: silentBan,
-		data: {
-			name: "silent-ban",
-			description: "Silently ban a user",
-			options: [
-				{
-					name: "user",
-					description: "User that should be banned",
-					type: ApplicationCommandOptionType.User,
-				},
-				{
-					name: "user-id",
-					description: "Id of the user that should be banned",
-					type: ApplicationCommandOptionType.String,
-				},
-			],
-		},
-	},
-	{
-		run: silentUnban,
-		data: {
-			name: "silent-unban",
-			description: "Unban a silently banned user",
-			options: [
-				{
-					name: "user",
-					description: "User that should be banned",
-					type: ApplicationCommandOptionType.User,
-				},
-				{
-					name: "user-id",
-					description: "Id of the user that should be banned",
-					type: ApplicationCommandOptionType.String,
-				},
-			],
-		},
-	},
-	{
-		run: addChannelMessageKeeper,
-		data: {
-			name: "add-channel-message-keeper",
-			description: "Add a message keeper to a channel",
-			options: [
-				{
-					name: "channel",
-					description: "The channel",
-					type: ApplicationCommandOptionType.Channel,
-					required: true,
-				},
-			],
-		},
-	},
-	{
-		run: removeChannelMessageKeeper,
-		data: {
-			name: "remove-channel-message-keeper",
-			description: "Remove a message keeper from a channel",
-			options: [
-				{
-					name: "channel",
-					description: "The channel",
-					type: ApplicationCommandOptionType.Channel,
-					required: true,
-				},
-			],
-		},
-	},
+   {
+      run: rename,
+      data: {
+         name: "rename",
+         description: "Rename a user",
+         options: [
+            {
+               name: "user",
+               description: "The user you want to rename",
+               type: ApplicationCommandOptionType.User,
+               required: true,
+            },
+            {
+               name: "nickname",
+               description: "The new nickname",
+               type: ApplicationCommandOptionType.String,
+               required: true,
+            },
+         ],
+      },
+   },
+   {
+      run: createJoinLeaveMessage,
+      data: {
+         name: "create-join-leave-message",
+         description: "Join messages",
+         options: [
+            {
+               name: "type",
+               description: "Join, Leave, Kick, Ban",
+               type: ApplicationCommandOptionType.String,
+               choices: [
+                  { name: "join", value: "join" },
+                  { name: "leave", value: "leave" },
+                  { name: "kick", value: "kick" },
+                  { name: "ban", value: "ban" },
+               ],
+               required: true,
+            },
+            {
+               name: "channel",
+               description: "The channel the message should be send to",
+               type: ApplicationCommandOptionType.Channel,
+               required: true,
+            },
+            {
+               name: "message",
+               description: "{{user}} - username",
+               type: ApplicationCommandOptionType.String,
+               required: true,
+            },
+         ],
+      },
+   },
+   {
+      run: silentBan,
+      data: {
+         name: "silent-ban",
+         description: "Silently ban a user",
+         options: [
+            {
+               name: "user",
+               description: "User that should be banned",
+               type: ApplicationCommandOptionType.User,
+            },
+            {
+               name: "user-id",
+               description: "Id of the user that should be banned",
+               type: ApplicationCommandOptionType.String,
+            },
+         ],
+      },
+   },
+   {
+      run: silentUnban,
+      data: {
+         name: "silent-unban",
+         description: "Unban a silently banned user",
+         options: [
+            {
+               name: "user",
+               description: "User that should be banned",
+               type: ApplicationCommandOptionType.User,
+            },
+            {
+               name: "user-id",
+               description: "Id of the user that should be banned",
+               type: ApplicationCommandOptionType.String,
+            },
+         ],
+      },
+   },
+   {
+      run: addChannelMessageKeeper,
+      data: {
+         name: "add-channel-message-keeper",
+         description: "Add a message keeper to a channel",
+         options: [
+            {
+               name: "channel",
+               description: "The channel",
+               type: ApplicationCommandOptionType.Channel,
+               required: true,
+            },
+         ],
+      },
+   },
+   {
+      run: removeChannelMessageKeeper,
+      data: {
+         name: "remove-channel-message-keeper",
+         description: "Remove a message keeper from a channel",
+         options: [
+            {
+               name: "channel",
+               description: "The channel",
+               type: ApplicationCommandOptionType.Channel,
+               required: true,
+            },
+         ],
+      },
+   },
+   {
+      run: rockPaperSissors,
+      data: {
+         name: "rock-paper-sissors",
+         description: "Play rock-paper-sissors with me",
+         options: [
+            {
+               name: "selection",
+               description: "The opion you chose",
+               type: ApplicationCommandOptionType.String,
+               choices: [
+                  {
+                     name: "Rock",
+                     value: "rock",
+                  },
+                  {
+                     name: "Paper",
+                     value: "paper",
+                  },
+                  {
+                     name: "Sissors",
+                     value: "sissors",
+                  },
+               ],
+               required: true,
+            },
+         ],
+      },
+   },
 ];
 
 export default commands;
